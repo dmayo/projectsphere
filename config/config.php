87293e82bef642272faf174bae753f5e78288ca5
ashaw596 <ashaw596@gmail.com>
2014-12-19 16:59:22 -0500

    Add the db config to the main config file.

--- config/config.php
+++ config/config.php
@@ -1,4 +1,8 @@
 <?php
 	//require dirname(__FILE__).'../file
 	$GLOBALS['config_url'] = 'http://localhost/';
+	$GLOBALS['config_db_host'] = 'localhost';
+	$GLOBALS['config_db_name'] = 'project_sphere';
+	$GLOBALS['config_db_user'] = 'root';
+	$GLOBALS['config_db_pass'] = 'school';
 ?>
--- database/pdo.php
+++ database/pdo.php
@@ -1,8 +1,9 @@
 <?php	
+	require_once __DIR__.'/../config/config.php';
 	function getPDO() {
-		$dsn = 'mysql:host=localhost;dbname=project_sphere';
-		$user = 'root';
-		$password = 'school';
+		$dsn = 'mysql:host='.$GLOBALS['config_db_host'].';dbname='.$GLOBALS['config_db_name'];
+		$user = $GLOBALS['config_db_user'];
+		$password = $GLOBALS['config_db_pass'];
 		try {
 			$pdo = new PDO($dsn, $user, $password);
 			$pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
